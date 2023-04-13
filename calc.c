8a81c60dc583080e369cadc9600b51fa09ea0325
0finderfail <50328441+Shadowmaster346@users.noreply.github.com>
2023-04-13 20:02:03 +0300

    Calculator on C
    Fork of my old project (calculator on bash)

---
+++ calc.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+float fst;
+float scd;
+char ndm;
+char plus = "+";
+printf("Small calculator\n");
+printf("Type your first number\n");
+scanf("%f", &fst);
+printf("You First number is ");
+printf("%f\n",fst);
+printf("Type your second number\n");
+scanf("%f", &scd);
+printf("You Second number is %f\n", scd);
+printf("Choose an operator: +-*/: ");
+scanf("%s", &ndm);
+
+switch(ndm){
+case '+':
+printf("%f\n", fst+scd);
+break;
+case '-':
+printf("%f\n", fst-scd);
+break;
+}
+
+return 0;
+}
